8be8c02262440321df441e38145092734f719c18
Yahia_M <59213878+Yahia-M@users.noreply.github.com>
2021-12-26 00:10:23 +0100

    Connect to database

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from sqlalchemy.engine import create_engine
 from core.config import settings
-
-app = FastAPI(title=settings.PROJECT_TITLE,varion=settings.PROJECT_VERSION)
-
-
+from db.session import engine
+from db.base_class import Base
 
 
-app.get("/")
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE,varion=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
+
+
+@app.get("/")
 def hello_api():
     return {"detail":"Hello Worlds"}
+
